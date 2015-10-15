f01d354500273ba1e77d20955538c0e9cab33813
Pierre <pierre.biolley@etu.udamail.fr>
2015-10-16 01:52:52 +0200

    add button

---
+++ Afro-Viking/Assets/Game/Scripts/Others/Button/AActivable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AActivable : MonoBehaviour {
+
+	protected bool isActivate;
+	virtual internal void Activate(bool activate)
+	{
+		isActivate = activate;
+	}
+}
---
+++ Afro-Viking/Assets/Game/Scripts/Others/Button/GameButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameButton : MonoBehaviour {
+
+	public AActivable Button = null;
+	public AActivable Activable = null;
+	
+	void OnCollisionEnter2D(Collision2D a_collider)
+	{
+		if (a_collider.gameObject.tag == "Player")
+		{
+			Button.Activate(true);
+			Activable.Activate(true);
+		}
+	}
+	
+	void OnCollisionExit2D(Collision2D a_collider)
+	{
+		if (a_collider.gameObject.tag == "Player")
+		{
+			Button.Activate(false);
+			Activable.Activate(false);
+		}
+	}
+}
